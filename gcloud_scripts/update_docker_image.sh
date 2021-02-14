5d1789b9c9628614cbc17b3b884707fa2ed87674
pmalonis <pjmalonis@gmail.com>
2021-02-14 17:42:37 -0500

    adding my account to update_docker_image.sh

--- gcloud_scripts/update_docker_image.sh
+++ gcloud_scripts/update_docker_image.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-docker_tag="200402"
+docker_tag="000001"
 base_name="pbtclient"
-u='snelbeta'
-p='$Cr}ZqK:mg68)Vny'
+u='pmalonis'
+p='gQ^WznWU&-#9Luq'
 docker_login="docker login --username='$u' --password='$p'"
-docker_pull="docker pull snelbeta/autolfads:${docker_tag}"
+docker_pull="docker pull pmalonis/autolfads:${docker_tag}"
 
 for instance in $(gcloud compute instances list --filter="tags:$base_name AND STATUS:RUNNING" --format="csv[no-heading](name)")
 
